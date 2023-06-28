fbe82d0474ae83bce1877b72aacb0969dcc083f8
Diksha <dikshabuxani84745@gmail.com>
2023-06-28 20:14:18 +0530

    home page display

---
+++ src/pages/Home.jsx
@@ -0,0 +1,167 @@
+import React from 'react'
+import { FaRupeeSign } from 'react-icons/fa';
+import { BsPeopleFill } from 'react-icons/bs';
+import { dashboard } from '../assests';
+import {AiFillTag} from 'react-icons/ai'
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import {BsCalendarDateFill} from 'react-icons/bs'
+const Home = () => {
+  return (
+    <div className='flex w-[99%] h-[88%] border m-4 bg-[white] flex-col'>
+
+
+      <div className='flex  w-[97.5%] m-5 justify-end gap-[48rem]'>
+        <span className=' text-[25px] font-[500]'>Events</span>
+
+        <div class="flex   items-center">
+
+          <label for="simple-search" class="sr-only">Search</label>
+          <div class="relative w-full">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+          </div>
+          <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class=" w-[10rem] p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Add New
+          </button>
+        </div>
+      </div>
+
+      <div className='flex flex-row m-5 w-[97%] h-[12rem] border-[2px] items-center justify-center gap-2 p-2'>
+
+
+        <div className='w-[15%] h-[90%] ' >
+          <img src={dashboard} className="w-[100%] h-[100%]" alt="" srcset="" />
+        </div>
+
+        <div className='w-[30%] h-[90%]  flex flex-col  items-start pl-5 pt-3  gap-3'>
+
+          <h1 className='text-[25px] font-[400] h-[2.5rem]'>Stuck in sound</h1>
+
+          <span>
+            Synolo
+          </span>
+
+          <span className='bg-[#F2F9FF] text-[#74BFFF] border p-1 rounded-lg'>
+            ongoing
+          </span>
+
+        </div>
+
+        <div className='w-[30%] h-[90%]  flex flex-col  items-start pl-5 pt-[3.5rem]  gap-3'>
+          <span className='flex justify-center items-center gap-1'>
+            <FaRupeeSign />
+            30,000 in prizes
+          </span>
+
+          <span className='flex justify-center items-center gap-1' >
+            <BsPeopleFill />
+            256 registered
+          </span>
+        </div>
+
+        <div className=' flex flex-col w-[20%] h-[90%] border-l-2 pt-3 gap-[1rem]'>
+          <span className='flex justify-center items-center gap-3 text-[17px] w-[90%]'>
+            <AiFillTag />
+            Hackathon
+          </span>
+
+          <span className='flex justify-center items-center gap-3 text-[17px] w-[90%]' >
+            <HiOutlineLocationMarker />
+            Courtyard
+          </span>
+
+          <span className='flex justify-center items-center gap-3 text-[17px] w-[90%]' >
+            <BsCalendarDateFill />
+            04-07-2023
+          </span>
+        </div>
+      </div>
+
+      {/* <!-- Modal toggle --> */}
+
+      <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-md max-h-full">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
+              <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+            <div class="px-6 py-6 lg:px-8">
+              <form class="space-y-6" action="#">
+                <div>
+                  <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event Title</label>
+                  <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                </div>
+                <div>
+                  <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                  <textarea rows={5} type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                </div>
+
+                <div >
+                  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload Image</label>
+                  <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+                </div>
+
+                <div >
+                  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload Banner Image</label>
+                  <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+                </div>
+
+                <div className='flex gap-4 items-center'>
+                  <h3 class="mb-4 font-semibold  w-[8rem] text-gray-900 dark:text-white">Event Mode</h3>
+                  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Offline</label>
+                      </div>
+                    </li>
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="react-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Online</label>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+
+                <div>
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="dropdownDefaultButton">Event Type</label>
+
+                  <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black bg-white-700 hover:border-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Select Event Type <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                  {/* <!-- Dropdown menu --> */}
+                  <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                      <li>
+                        <a href="/#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                      </li>
+                      <li>
+                        <a href="/#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                      </li>
+                      <li>
+                        <a href="/#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                      </li>
+                      <li>
+                        <a href="/#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="flex items-center  space-x-2 border-gray-200 rounded-b dark:border-gray-600">
+                  <button data-modal-hide="authentication-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+                  <button data-modal-hide="authentication-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Proceed</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
