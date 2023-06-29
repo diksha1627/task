507313dcec6ba4f47eb69f0f4c8fae7a6d4825ce
Diksha <dikshabuxani84745@gmail.com>
2023-06-29 23:04:02 +0530

    another form added for participation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRupeeSign } from 'react-icons/fa';
 import { BsPeopleFill } from 'react-icons/bs';
 import { dashboard } from '../assests';
-import {AiFillTag} from 'react-icons/ai'
-import {HiOutlineLocationMarker} from 'react-icons/hi'
-import {BsCalendarDateFill} from 'react-icons/bs'
+import { AiFillTag } from 'react-icons/ai'
+import { HiOutlineLocationMarker } from 'react-icons/hi'
+import { BsCalendarDateFill } from 'react-icons/bs'
 const Home = () => {
+   const [show,setShow]=useState(false);
+  const handleshow = ()=>
+  {
+     setShow(!show);
+  }
   return (
     <div className='flex w-[99%] h-[88%] border m-4 bg-[white] flex-col'>
 
@@ -128,7 +133,7 @@ const Home = () => {
                 </div>
 
                 <div>
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="dropdownDefaultButton">Event Type</label>
+                  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="dropdownDefaultButton">Event Type</label>
 
                   <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black bg-white-700 hover:border-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Select Event Type <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                   {/* <!-- Dropdown menu --> */}
@@ -152,7 +157,107 @@ const Home = () => {
 
                 <div class="flex items-center  space-x-2 border-gray-200 rounded-b dark:border-gray-600">
                   <button data-modal-hide="authentication-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
-                  <button data-modal-hide="authentication-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Proceed</button>
+                  <button data-modal-target="authentication-modal2" data-modal-toggle="authentication-modal2" data-modal-hide="authentication-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Next</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div id="authentication-modal2" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-md max-h-full">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+            <div class="px-6 py-6 lg:px-8">
+              <form class="space-y-6" action="#">
+                <div className='flex mt-1'>
+                  <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Participation Type</label>
+                  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Individual</label>
+                      </div>
+                    </li>
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="react-checkbox-list" onClick={handleshow} type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="react-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Group</label>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div className={show  ? 'flex' : 'hidden'}>
+                  <label for="description" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Members</label>
+                  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input type="number" value="" placeholder="Min" class="placeholder:text-[0.8rem] placeholder:pl-20 w-full h-full text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                      </div>
+                    </li>
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input type="number" value="" placeholder="Max" class="placeholder:text-[0.8rem] placeholder:pl-20 w-full h-full text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="flex gap-2">
+                  <label class="block mt-3 w-[70%] text-sm font-medium text-gray-900 dark:text-white" for="startdate">Start Date and Time</label>
+                  <input id="startdate" name="startdate" type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date and time" />
+                </div>
+
+                <div class="flex gap-2">
+                  <label class="block mt-3 w-[70%] text-sm font-medium text-gray-900 dark:text-white" for="enddate">End Date and Time</label>
+                  <input id="enddate" name="enddate" type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date and time" />
+                </div>
+
+                <div className='flex gap-4 items-center'>
+                  <h3 class="mb-4 font-semibold  w-[10rem] text-gray-900 dark:text-white">Judging Mode</h3>
+                  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="vue-checkbox-list" class="w-full py-3 ml-2  font-medium text-gray-900 dark:text-gray-300">Likes</label>
+                      </div>
+                    </li>
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="react-checkbox-list" class="w-full py-3 ml-2  font-medium text-gray-900 dark:text-gray-300">Judging</label>
+                      </div>
+                    </li>
+                    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                      <div class="flex items-center pl-3">
+                        <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                        <label for="react-checkbox-list" class="w-full py-3 ml-2  font-medium text-gray-900 dark:text-gray-300">Hybrid</label>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className='flex gap-3'>
+                  <label class="block mt-2  font-medium text-gray-900 dark:text-white" for="location">Location</label>
+                  <input type="text" name="title" id="title" placeholder='Enter event location' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                </div>
+
+                <div className='flex gap-3'>
+                  <label class="block   font-medium text-gray-900 dark:text-white" for="location">Private</label>
+
+                  <label class="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer" />
+                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                  </label>
+                </div>
+
+
+
+                <div class="flex items-center  space-x-2 border-gray-200 rounded-b dark:border-gray-600">
+                  <button data-modal-hide="authentication-modal2" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+                  <button data-modal-hide="authentication-modal2" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Proceed</button>
                 </div>
               </form>
             </div>
